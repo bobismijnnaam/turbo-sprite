0d67fb906f7ed4c4a11060c276dcbc1ba369a4d6
Bob Rubbens <bobrubbens@gmail.com>
2014-01-07 23:49:13 +0100

    Finished the header for AnimatedSprite class

---
+++ AnimatedSprite.cpp
@@ -0,0 +1,16 @@
+// Author: Bob Rubbens - Knights of the Compiler 
+// Creation date: di 07-01-2014 
+// Contact: http://plusminos.nl - @broervanlisa - gmail (bobrubbens) 
+
+// Includes I didn't make myself 
+
+ // Includes I did make myself 
+#include "AnimatedSprite.hpp" 
+
+AnimatedSprite(std::string config) {
+
+}
+
+AnimatedSprite::~AnimatedSprite() {
+
+}
---
+++ AnimatedSprite.hpp
@@ -0,0 +1,81 @@
+// Author: Bob Rubbens - Knights of the Compiler
+// Creation date: di 07-01-2014
+// Contact: http://plusminos.nl - @broervanlisa - gmail (bobrubbens)
+
+#ifndef ANIMATEDSPRITE_HPP
+#define ANIMATEDSPRITE_HPP
+
+// Includes I didn't make myself
+#include <queue>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+ // Includes I did make myself
+
+namespace nb {
+	struct Animation {
+		// The collection of frames stored as 2d coordinates
+		std::vector<sf::Vector2i> frames;
+		// The time between the frames
+		int dt;
+		// The next animation that should be played
+		std::string next;
+	} ;
+
+	class AnimatedSprite : public sf::Transformable, public sf::Drawable {
+	public:
+		// Loads an animated sprite using an external configuration file
+		// Sets initial frame to the first frame
+		// See example file for more details
+		AnimatedSprite(std::string config);
+		~AnimatedSprite();
+
+		// Sets the spritesheet to be used for rendering. Removes the previous spritesheet
+		void setSpriteSheet(sf::Texture* inputSpritesheet);
+
+		// Performs the logic of the animated sprite (mostly just updating the animation/frame)
+		void logic(float dt);
+		// Draws the animated sprite
+		void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+		// Starts a certain animation
+		void play(std::string animation);
+		// Resumes the animation if it was paused. Also resets the timer
+		void resume();
+		// Queues another animation. Once the current animation ends, this one is started
+		void queue(std::string animation);
+		// Pauses the current animation
+		void pause();
+		// Sets the sprite to a certain frame and pauses it
+		void set(int frame);
+
+	private:
+		// Contains the individual animations
+		std::unordered_map<std::string, Animation> animations;
+		// Contains the animations to be played
+		std::queue animationQueue;
+
+		// The spritesheet 
+		sf::Texture* spritesheet;
+		// The size of the sprite
+		sf::Vector2f spriteSize;
+		// The amount of frames over the width & height of the spritesheet
+		// (The amount that fits in along the x & y axis)
+		sf::Vector2f gridSize;
+
+		// The current animation
+		Animation currentAnimation;
+		// The current frame
+		int currentFrame;
+
+		// Whether an animation is playing right now or not
+		bool playing;
+		// How much time has elapsed since the last frame switch
+		sf::Time elapsed;
+	};
+
+}
+
+#endif
